f2fcf5302328f583da24e112bb7919403bfa3921
vdto <vivdto@gmail.com>
2023-05-01 00:09:51 +0530

    Added the UI Part using React JS

--- api/index.js
+++ api/index.js
@@ -42,6 +42,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+app.listen("5001", () => {
   console.log("Backend is running.");
 });
